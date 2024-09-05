cd70d5cf9a54222680f5bc0c6757e1d3d29f80e6
Soso-Mabulu <solulelekwani@gmail.com>
2024-09-05 22:41:11 +0200

    allowd headers on cors modified

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cors({
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['*'],
   credentials: true,
 }));
 
